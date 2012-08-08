d9f12f83ca40d1eb8d71162caa89c9e4c651593b
Nashova <nashova@kanzen.co>
2012-08-08 17:01:54 -0500

    Creación de Un Hello World en C++

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+ * HelloWorld.cpp
+ *
+ *  Created on: 08/08/2012
+ *      Author: Admin
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+   cout << "Hello, world!" << endl;
+   return 0;
+}
